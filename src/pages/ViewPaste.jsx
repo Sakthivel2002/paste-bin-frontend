ef51f47f5b0505fcb1555f27d864d53c7d0a2069
Sakthivel P <sakthirollins175@gmail.com>
2026-02-03 13:19:43 +0530

    Update ViewPaste.jsx

--- src/pages/ViewPaste.jsx
+++ src/pages/ViewPaste.jsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { API } from './api'
+import { API } from '../api'
 
 export default function ViewPaste() {
   const { id } = useParams()
